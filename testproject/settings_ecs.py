a77ddbbfbefd06dabde5b7828fa70ac1927e80f5
shanjidhasan <shanjidhasan202@gmail.com>
2025-08-24 04:03:06 +0600

    Log ALLOWED_HOSTS at startup and on DisallowedHost exception for debugging

--- testproject/settings_ecs.py
+++ testproject/settings_ecs.py
@@ -2,6 +2,9 @@
 import os
 from pathlib import Path
 
+import logging
+logger = logging.getLogger('django.allowed_hosts')
+
 from .settings import *  # Import all default settings
 
 # Override database settings for ECS if environment variables are set
@@ -22,3 +25,14 @@ if _allowed_hosts_env.strip() == '*':
     ALLOWED_HOSTS = ['*']
 else:
     ALLOWED_HOSTS = [h.strip() for h in _allowed_hosts_env.split(',') if h.strip()]
+
+logger.info(f"ALLOWED_HOSTS at startup: {ALLOWED_HOSTS}")
+
+# Patch Django's DisallowedHost exception to log the host being checked
+import django.core.exceptions
+_orig_disallowed_host = django.core.exceptions.DisallowedHost
+class LoggedDisallowedHost(_orig_disallowed_host):
+    def __init__(self, msg, *args, **kwargs):
+        logger.error(f"DisallowedHost check failed: {msg} | ALLOWED_HOSTS: {ALLOWED_HOSTS}")
+        super().__init__(msg, *args, **kwargs)
+django.core.exceptions.DisallowedHost = LoggedDisallowedHost
